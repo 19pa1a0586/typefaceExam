fb2add9b43cbc6fba439e29c77df63025eb68370
19pa1a0586 <60594951+19pa1a0586@users.noreply.github.com>
2022-12-13 21:07:10 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,18 @@
+from gtts import gTTS
+def getSound(word):
+    mytext = word
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("newSound.mp3")
+    
+ct = 0
+file_name = input()
+req_file = file_name+".txt"
+wd = getSound(wd)
+rel_words = []
+with open(req_file,'r') as file:
+    for line in file:
+        for word in line.split():
+            if getSound(word) == wd:
+                rel_words.append(word)
+print(rel_words)
